1b14915a28ea9c816ef4d0c18a5703b19518e48e
Arpit Gupta <grpit.github@gmail.com>
2021-01-08 02:57:16 +0530

    Took out header component.

---
+++ components/header/index.tsx
@@ -0,0 +1,9 @@
+const Header = () => (
+  <header className='mb-4 py-2 px-8 shadow-lg bg-white flex items-center top-0 h-14 fixed w-full'>
+    <div className='container flex flex-row items-center mx-auto '>
+      <span className='text-2xl font-semibold'>gRPIT Blog</span>
+    </div>
+  </header>
+);
+
+export default Header;

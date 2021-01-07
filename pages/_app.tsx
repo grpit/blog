773297144a6d7df090991d2339da3cb7396c5678
Arpit Gupta <grpit.github@gmail.com>
2021-01-08 02:58:38 +0530

    Modified App component to structure better.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
+import Header from 'components/header';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className='absolute right-0 bg-gray-100 top-0 w-full h-full overflow-hidden'>
+      <Header />
+      <main className='h-full overflow-y-auto overflow-x-hidden mt-14'>
+        <div className='my-5 px-16 mx-auto container grid grid-cols-5 gap-4'>
+          <div className='col-span-1' />
+          <div className='col-span-3'>
+            <Component {...pageProps} />
+          </div>
+          <div className='col-span-1' />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default MyApp;

d3e5fa212ae01dbf4e906ad56eacb3491e51ed29
Arpit Gupta <grpit.github@gmail.com>
2021-01-18 03:25:54 +0530

    Finalized colors and added to tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: {
     content: [
@@ -7,10 +9,21 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    colors: {
+      ...colors,
+      primary: '#004966',
+      secondary: '#A26769',
+      'primary-text': colors.gray['900'],
+      title: '#004966',
+      subtitle: colors.gray['600'],
+      header: '#092434',
+      base: '#CAE5F6'
+    }
   },
   variants: {
     extend: {}
   },
   plugins: []
 };
+
+// palette url https://coolors.co/f7f9f9-d9e8e8-78d5d7-004966-2285bf-a26769

46adc7ff9b051473a377ebf3e91c55b9c14d4f07
Arpit Gupta <grpit.github@gmail.com>
2021-01-19 03:05:56 +0530

    Modified _app page to use Layout Resolver.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,13 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
-import Header from 'components/header';
+import LayoutResolver from 'utils/LayoutResolver';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className='absolute right-0 bg-base top-0 w-full h-full overflow-hidden'>
-      <Header title='gRPIT Blog' />
-      <main className='h-full overflow-y-auto overflow-x-hidden mt-14'>
-        <div className='my-5 px-16 mx-auto container grid grid-cols-5 gap-4'>
-          <div className='col-span-1' />
-          <div className='col-span-3'>
-            <Component {...pageProps} />
-          </div>
-          <div className='col-span-1' />
-        </div>
-      </main>
+      <LayoutResolver>
+        <Component {...pageProps} />
+      </LayoutResolver>
     </div>
   );
 }
